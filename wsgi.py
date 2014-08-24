524a9ac60e8619ab899baa4313268d58559c6a5c
farouk <farouk123@users.noreply.github.com>
2014-08-24 04:02:51 +0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ import bottle
 @bottle.route('/')
 def index():
     pyver = '.'.join(map(str, tuple(sys.version_info)[:3]))
-    return 'Hello! (from <b>Python %s</b>)' % (pyver,)
+    return 'جامعة العلوم والتقانة برنامج ماجستير نظم المعلومات! (from <b>Python %s</b>)' % (pyver,)
 
 
 application = bottle.default_app()
